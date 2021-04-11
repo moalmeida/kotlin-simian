fb328b38db5059e9a61377c94660e506beda7ea9
Mauro Almeida <mauro.almeida@loft.com.br>
2021-04-11 20:19:53 -0300

    initial commit

--- app/micronaut-app/src/main/kotlin/simian/exception/MicronautBadRequestExceptionHandler.kt
+++ app/micronaut-app/src/main/kotlin/simian/exception/MicronautBadRequestExceptionHandler.kt
@@ -6,13 +6,19 @@ import io.micronaut.core.exceptions.ExceptionHandler
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Produces
+import io.micronaut.http.server.exceptions.response.HateoasErrorResponseProcessor
+import io.micronaut.jackson.JacksonConfiguration
 import io.micronaut.validation.exceptions.ConstraintExceptionHandler
 import javax.validation.ConstraintViolationException
 
 @Produces
 @Replaces(ConstraintExceptionHandler::class)
 @Requires(classes = [ConstraintViolationException::class, ExceptionHandler::class, NotContainsOnlyNitroWordsException::class])
-class MicronautBadRequestExceptionHandler : ConstraintExceptionHandler() {
+class MicronautBadRequestExceptionHandler : ConstraintExceptionHandler(
+    HateoasErrorResponseProcessor(
+        JacksonConfiguration()
+    )
+) {
     override fun handle(request: HttpRequest<*>?, exception: ConstraintViolationException): HttpResponse<*> {
         return HttpResponse.badRequest<Any>()
     }
